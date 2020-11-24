809aeefad0a22e34e99dc7d396b72ea1ec899436
Herrius <65200014+Herrius@users.noreply.github.com>
2020-11-23 21:23:15 -0500

    Initial commit

---
+++ js/inte.js
@@ -0,0 +1,21 @@
+// const title= document.getElementById('presentar');
+// const par=document.querySelector('a')
+
+// console.log(title.getAttribute('content'))
+
+// title.style.background='hsl(160,50%,80%)'
+
+// // es la forma que si o si te va dar resultado
+// // par.setAttribute('id','title')
+
+// par.id="image"
+// console.log(par);
+
+// // muestra solamente texto
+// console.log(title.textContent)
+// // muestra texto y los hijos en html
+// console.log(title.innerHTML)
+// // te muestra todo el html del elemento
+// console.log(title.outerHTML)
+
+// title.innerHTML='hola  <strong>mundo</strong>'
